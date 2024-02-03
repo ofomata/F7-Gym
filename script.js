9d7b9862e2a1e00384b5bc8207cc641bb578f9a0
ofomata <miracleosita355@gmail.com>
2024-02-03 17:13:30 +0100

    about page

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ menu.addEventListener("click", () => {
     nav.classList.toggle("active-nav");
 });
 
+//Button Toggle
 
 const buttonOne = document.querySelector(".btn1");
 const buttonTwo = document.querySelector(".btn2");
@@ -32,3 +33,31 @@ buttonTwo.addEventListener("click", () => {
     buttonTwo.classList.add("clicked");
     buttonOne.classList.remove("clicked");
 });
+
+//CountDown
+
+const countDown = new Date("Feb 02, 2024 00:00:00").getTime();
+const x = setInterval(function(){
+    const now = new Date().getTime();
+    const distance = now - countDown;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    /* if(distance < 0){
+        clearInterval(x);
+
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    } */
+
+},1000);
